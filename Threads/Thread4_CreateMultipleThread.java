44ff453fd58402bc950d5f14d1e396a459a4a11e
seemakumariatgithub <70597160+seemakumariatgithub@users.noreply.github.com>
2024-05-20 20:04:37 +0530

    Create Thread4_CreateMultipleThread.java

---
+++ Threads/Thread4_CreateMultipleThread.java
@@ -0,0 +1,43 @@
+/*==================================
+AUTHOR : SEEMA KUMARI
+=====================================*/
+class Thread4_CreateMultipleThread {
+    public static void main(String[] args) {
+        new CreateThread("one");
+        new CreateThread("two");
+        new CreateThread("three");
+        
+        try{
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException e){
+            System.out.println("Main thread interrupted.");
+        }
+}
+}
+
+
+class CreateThread  implements Runnable{
+    Thread newThread;
+    String name;
+    
+    CreateThread(String threadName){
+        name=threadName;
+        newThread=new Thread(this,name);
+        System.out.println("Child Thread : "+newThread.getName());
+        newThread.start();
+    }
+    
+    public void run(){
+        try{
+            for(int i=5;i>0;i--){
+                System.out.println("Child : "+name+" => "+i);
+                Thread.sleep(500);
+            }
+        }
+        catch(InterruptedException e){
+            System.out.println("Child Thread "+name+" Interrupted.");
+        }
+        System.out.println(" Exits the Child "+name+" Thread.");
+    }
+}
