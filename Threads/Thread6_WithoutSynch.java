5b1d3532dff1446dd92c6db1f1da946923a89ed0
seemakumariatgithub <70597160+seemakumariatgithub@users.noreply.github.com>
2024-05-28 14:40:10 +0530

    Create Thread6_WithoutSynch.java

---
+++ Threads/Thread6_WithoutSynch.java
@@ -0,0 +1,47 @@
+class Thread6_Withoutynch{
+    public static void main(String args[]){
+        CallMe target=new CallMe();
+        Caller caller1=new Caller(target,"Hello");
+        Caller caller2=new Caller(target,"Synchronized");
+        Caller caller3=new Caller(target,"World");
+        
+        // Now we wait for above threads to end
+        try{
+            caller1.th.join();
+            caller2.th.join();
+            caller3.th.join();
+        }catch(InterruptedException e){
+            System.out.println("Interrupted");
+        }
+    }
+}
+
+class CallMe{
+    void call(String msg){
+        System.out.print("["+msg);
+        try{
+            Thread.sleep(1000);
+        }catch(InterruptedException e){
+            System.out.println("Interrupted");
+        }
+        System.out.println("]");
+    }
+}
+
+class Caller implements Runnable {
+    String msg;
+    CallMe target;
+    Thread th;
+    
+    Caller(CallMe myTarget,String myMsg){
+        target=myTarget;
+        msg=myMsg;
+        th=new Thread(this);
+        th.start();
+    }
+    
+    public void run(){
+        target.call(msg);
+    }
+}
+
