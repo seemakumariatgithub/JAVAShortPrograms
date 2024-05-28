89c927b1a47d70ec92c85a8500b1c36b6608bdb5
seemakumariatgithub <70597160+seemakumariatgithub@users.noreply.github.com>
2024-05-28 14:45:06 +0530

    Create Question2.java

---
+++ Java/CodeQuestions/Question2.java
@@ -0,0 +1,29 @@
+class HelloWorld{
+  public static void main(String[] args){
+    System.out.println(null+true);
+    System.out.println(true+null);
+    System.out.println(null+null);
+  }
+}
+
+/*===== this will throw compile tim error as 
+ERROR!
+/tmp/H2JhvENb9S/HelloWorld.java:3: error: bad operand types for binary operator '+'
+    System.out.println(null+true);
+                           ^
+  first type:  <null>
+  second type: boolean
+ERROR!
+/tmp/H2JhvENb9S/HelloWorld.java:4: error: bad operand types for binary operator '+'
+    System.out.println(true+null);
+                           ^
+  first type:  boolean
+  second type: <null>
+ERROR!
+/tmp/H2JhvENb9S/HelloWorld.java:5: error: bad operand types for binary operator '+'
+    System.out.println(null+null);
+                           ^
+  first type:  <null>
+  second type: <null>
+3 errors
+*/
