e61905fb35e7d4fa622e0ab0d0404a0a1741c66e
seemakumariatgithub <70597160+seemakumariatgithub@users.noreply.github.com>
2024-05-29 23:27:03 +0530

    Update Thread10_deamonThread.java

--- Threads/Thread10_deamonThread.java
+++ Threads/Thread10_deamonThread.java
@@ -1,1 +1,7 @@
+//one can only call the setDaemon() method before start() method otherwise it will definitely throw IllegalThreadStateException
+//We can create daemon threads in java using the thread class setDaemon(true).
+//There are basically two methods available in thread class for daemon thread: setDaemon() and isDaemon(). 
+// In Java, daemon threads are basically referred to as a service provider that provides services and support to user threads. 
+/*=================*/
+
 
