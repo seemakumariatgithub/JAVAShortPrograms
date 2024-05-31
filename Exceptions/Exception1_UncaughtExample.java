565438525dce417ce4d01b5172b204edf5b2f37e
seemakumariatgithub <70597160+seemakumariatgithub@users.noreply.github.com>
2024-05-31 16:43:49 +0530

    Create Exception1_UncaughtExample.java

---
+++ Exceptions/Exception1_UncaughtExample.java
@@ -0,0 +1,15 @@
+class Exception1_UncaughtExample{
+    public static void main(String[] args){
+        int a=0;
+        int b=67;
+        
+        int c= b/a;
+    }
+}
+
+/* This will throw below mentioned exception
+
+Exception in thread "main" java.lang.ArithmeticException: / by zero
+	at Exception1_UncaughtExample.main(Exception1_UncaughtExample.java:6)
+ 
+*/
