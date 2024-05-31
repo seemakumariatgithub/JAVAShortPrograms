103605f06eac2d826cc1e3e0370ce9cca21afe50
seemakumariatgithub <70597160+seemakumariatgithub@users.noreply.github.com>
2024-05-31 16:47:28 +0530

    Create Exception1_UncaughtStackTraceExample.java

---
+++ Exceptions/Exception1_UncaughtStackTraceExample.java
@@ -0,0 +1,20 @@
+class Exception1_UncaughtStackTraceExample{
+    public static void main(String[] args){
+        anAction();
+    }
+    
+    static void anAction(){
+        int a=0;
+        int b=67;
+        
+        int c= b/a;
+    }
+}
+
+/*This will throw the exception in below mentioned format
+
+Exception in thread "main" java.lang.ArithmeticException: / by zero
+	at Exception1_UncaughtExample.anAction(Exception1_UncaughtExample.java:10)
+	at Exception1_UncaughtExample.main(Exception1_UncaughtExample.java:3)
+
+*/
