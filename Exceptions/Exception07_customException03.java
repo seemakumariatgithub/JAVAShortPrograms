28f63bd713e6ada18b0a19c1fc6a78f1f0d2f295
seemakumariatgithub <70597160+seemakumariatgithub@users.noreply.github.com>
2024-06-02 09:42:14 +0530

    Create Exception07_customException03.java

---
+++ Exceptions/Exception07_customException03.java
@@ -0,0 +1,30 @@
+class Exception07_customException03{
+    
+    public static void main(String[] args){
+        try{
+            throw new MyCustomException();
+        }catch(MyCustomException ex){
+            System.out.println("In Main: caught "+ex.getMessage());
+        }
+    }
+    
+}
+
+class MyCustomException extends Exception{
+    
+    MyCustomException(String exMsg){
+        super(exMsg);
+    }
+    
+}
+
+/*OUTPUT: will be 
+ERROR!
+/tmp/xXPecNDt6X/Exception07_customException.java:5: error: constructor MyCustomException in class MyCustomException cannot be applied to given types;
+            throw new MyCustomException();
+                  ^
+  required: String
+  found: no arguments
+  reason: actual and formal argument lists differ in length
+1 error
+*/
