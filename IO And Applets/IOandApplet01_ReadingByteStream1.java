d55a5a162891cc3ad74104c855d5b275473fb015
seemakumariatgithub <70597160+seemakumariatgithub@users.noreply.github.com>
2024-06-07 21:37:49 +0530

    Update IOandApplet01_ReadingByteStream1.java

--- IO And Applets/IOandApplet01_ReadingByteStream1.java
+++ IO And Applets/IOandApplet01_ReadingByteStream1.java
@@ -6,7 +6,7 @@ class IOandApplet01_ReadingByteStream1{
     public static void main(String[] args) {
         BufferedReader myreader=new BufferedReader(new InputStreamReader(System.in));
         try{
-                System.out.println(myreader.read());
+                System.out.println(myreader.read());// read() method returns the integer value i.e ASCII value
         }catch(Exception e){
             System.out.println(e);
         }
