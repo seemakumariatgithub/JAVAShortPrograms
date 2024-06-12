480a3f96e052f93ac9aa23583f88e1d4b3e9b5e6
seemakumariatgithub <70597160+seemakumariatgithub@users.noreply.github.com>
2024-06-12 18:02:49 +0530

    Create String01_Constructors.java

---
+++ String/String01_Constructors.java
@@ -0,0 +1,18 @@
+import static java.lang.System.out;
+public class String01_Constructors{
+    public static void main(String[] args){
+        String str1=new String("Constructor String(\"string\")");
+        
+        char[] ch={'C','o','n','s','t','r','u','c','t','o','r',' ','f','o','r',' ','s','u','b','s','t','r','i','n','g'};
+        String str2=new String(ch);
+        String str3=new String(ch,16,9);
+        
+        String stringObject=new String("Constructor String(String strObj)");
+        
+        String str4=new String(stringObject);
+        out.println(str1);
+        out.println(str2); 
+        out.println(str3);
+        out.println(str4);
+        }
+}
