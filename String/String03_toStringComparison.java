cd2020124c355461753bcf82987bed04a0e5f588
seemakumariatgithub <70597160+seemakumariatgithub@users.noreply.github.com>
2024-06-19 15:24:22 +0530

    Create String03_toStringComparison.java

---
+++ String/String03_toStringComparison.java
@@ -0,0 +1,30 @@
+public class String03_toStringComparison{
+    public static void main(String[] args){
+        String s1="hello";
+        String s2="Hello";
+        String s3="hello";
+        String s4=s3;
+        String s5=new String("hello");
+        
+         System.out.println("s1=hello.equals(s2=Hello) => "+ s1.equals(s2));
+        System.out.println("s1=hello.equals(s3=hello) => "+ s1.equals(s3));
+        System.out.println("s1=hello.equals(s4=s3) => "+ s1.equals(s4));
+          System.out.print("s1=hello.equals((s5=new String(hello))) => ");
+        System.out.println(s1.equals(s5));
+        System.out.print("new String(hello).equals(String(hello)) => ");
+        System.out.println(new String("hello").equals(new String("hello")));
+        System.out.println("==============================");
+        System.out.print("s1=hello == (s2=Hello) => ");
+        System.out.println(s1==s2);
+        System.out.print("s1=hello == (s3=hello) => ");
+        System.out.println(s1==s3);
+        System.out.print("s1=hello == (s4=s3) => ");
+        System.out.println(s1==s4);
+        System.out.print("s1==s1 => ");
+        System.out.println(s1==s1);
+        System.out.print("s1=hello == (s5=new String(hello)) => ");
+        System.out.println(s1==s5);
+        System.out.print("new String(hello)==new String(hello) => ");
+        System.out.println(new String("hello")==new String("hello"));
+        }
+}
