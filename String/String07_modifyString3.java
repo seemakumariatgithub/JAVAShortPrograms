5047fcf8e889289f65a4d6a1fe70d48d92aad129
seemakumariatgithub <70597160+seemakumariatgithub@users.noreply.github.com>
2024-06-23 13:23:25 +0530

    Create String07_modifyString3.java

---
+++ String/String07_modifyString3.java
@@ -0,0 +1,25 @@
+
+import static java.lang.System.out;
+public class String07_modifyString3{
+    public static void main(String[] args){
+         String myString="Now is ";
+         String myString2="the time.";
+         int startIndex=myString.indexOf("is");
+         out.println(startIndex);
+         out.print("myString.substring(startIndex) => ");
+         out.println(myString.substring(startIndex));
+         
+         
+         String myConcatString=myString.concat(myString2);
+         out.println("\nConcatinated String.");
+         out.println("myString.concat(myString2) : "+myConcatString);
+         
+         out.println("\nconcat() creates new object .");
+         out.print("myString.concat(myString2)==(myString+myString2) : ");
+         out.println(myString.concat(myString2)==(myString+myString2));
+         out.println("myConcatString.equals((myString+myString2)) : "+myConcatString.equals((myString+myString2)));
+         
+         out.println("\n Replacing the String.");
+         out.println("myString.replace(\"Now\",\"Today\") : "+myString.replace("Now","Today"));
+    }
+}
