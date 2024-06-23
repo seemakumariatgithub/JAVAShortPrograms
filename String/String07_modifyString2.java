d833871f92b621fd4b0711dc5c9cd25090188f45
seemakumariatgithub <70597160+seemakumariatgithub@users.noreply.github.com>
2024-06-23 13:28:29 +0530

    Update String07_modifyString2.java

--- String/String07_modifyString2.java
+++ String/String07_modifyString2.java
@@ -8,13 +8,13 @@ public class String07_modifyString2{
          out.print("myString.substring(startIndex) => ");
          out.println(myString.substring(startIndex));
          
-         
          String myConcatString=myString.concat(myString2);
          out.println("\nConcatinated String.");
-         out.println(myConcatString);
+         out.println("myString.concat(myString2) : "+myConcatString);
          
          out.println("\nconcat() creates new object .");
-         out.println(myConcatString==(myString+myString2));
-         out.println(myConcatString.equals((myString+myString2)));
+         out.print("myString.concat(myString2)==(myString+myString2) : ");
+         out.println(myString.concat(myString2)==(myString+myString2));
+         out.println("myConcatString.equals((myString+myString2)) : "+myConcatString.equals((myString+myString2)));
     }
 }
