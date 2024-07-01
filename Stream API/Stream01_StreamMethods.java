ff00f2e5565cdeff59a79704669f72012ad8c44f
seemakumariatgithub <70597160+seemakumariatgithub@users.noreply.github.com>
2024-07-01 16:17:59 +0530

    Update Stream01_StreamMethods.java

--- Stream API/Stream01_StreamMethods.java
+++ Stream API/Stream01_StreamMethods.java
@@ -2,21 +2,30 @@ import java.util.Arrays;
 import java.util.*;
 import static java.lang.System.out;
 import java.util.List;
-import java.util.Optional;
+import java.util.Optional; 
+public class Stream01_StreamMethods {  
+   public static void main(String[] args) {
  
-public class Stream01_StreamMethods { 
- 
-   public static void main(String[] args) { 
- 
-    List<Integer> myList=Arrays.asList(11,21,33,14,25,6,27);
+    List<Integer> myList=Arrays.asList(11,21,33,14,25,6,27,51,67,99);
     out.println("Original List: "+myList);
+ 
+    out.println("Maximum Value in list : "+myList.stream().max(Integer::compare));
+    out.println("Count element in list : "+myList.stream().count());
+    out.print("Sorted list :");
+    myList.stream()
+    .sorted()
+    .forEach(n -> out.print(n+", "));
     
-    Optional<Integer> maxValue=myList.stream().max(Integer::compare);
-    //out.println("Maximum Value : "+maxValue);
-    out.println("Find Maximum Value : "+myList.stream().max(Integer::compare));
-    out.println("Count Array Element as : "+myList.stream().count());
-    out.println("Sorted Array :"+
-    myList.stream().forEach(System.out::println));
+    out.print("\n\nFilter the even values : ");
+    myList.stream()
+    .filter(n -> n%2==0)
+    .forEach(n-> out.print(n+ ", "));
+    
+    out.print("\nFilter odd value greater than 30: ");
+    myList.stream()
+    .filter(n -> n%2!=0)
+    .filter(n -> n>30)
+    .forEach(n -> out.print(n+", "));  
  
    } 
 } 
