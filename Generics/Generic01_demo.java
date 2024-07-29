8d0bca660f907d972265f6821455a638f5822da5
seemakumariatgithub <70597160+seemakumariatgithub@users.noreply.github.com>
2024-07-29 18:20:46 +0530

    Create Generic01_demo.java

---
+++ Generics/Generic01_demo.java
@@ -0,0 +1,19 @@
+import static java.lang.System.out;
+
+public class Generic01_demo{
+    
+    public static void main(String[] args){
+        Generic_class<Integer> genClass= new Generic_class<Integer>(8);
+        genClass.showObjType();        
+    }
+}
+class Generic_class<T>{
+    T obj;
+    Generic_class(T ob){
+        obj=ob;
+    }
+    public void showObjType(){
+        out.println("Type of T bject is : "+ obj.getClass().getName());
+    }
+}
+
