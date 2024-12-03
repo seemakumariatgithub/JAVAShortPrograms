464bad80a789ef45ca81cc09a28a94c2b563f444
seemakumariatgithub <70597160+seemakumariatgithub@users.noreply.github.com>
2024-12-03 21:12:49 +0530

    Update removeDuplicatefromSorted array.java

--- Java/CodeQuestions/removeDuplicatefromSorted array.java
+++ Java/CodeQuestions/removeDuplicatefromSorted array.java
@@ -17,4 +17,18 @@ class Main {
         int result=tempSet.size();        
         return result;
     }
+
+    ////////BETTER WAY O(1)
+    class Solution {
+    public int removeDuplicates(int[] nums) {
+      int i=0;
+        for(int j=1;j<nums.length;j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+    }
+}
 }
